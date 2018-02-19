93678452e871e30c8193394a0d31ab6aeca6f875
Ali B <abittin@gmail.com>
2018-02-19 11:57:18 +0300

    dumpling: remove redundant bootanim scale enteries
    Change-Id: I7a30400fa8a30afd1992ebd358fed4f71e6e1a3b

--- device.mk
+++ device.mk
@@ -31,10 +31,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2160
-TARGET_SCREEN_WIDTH := 1080
-
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
